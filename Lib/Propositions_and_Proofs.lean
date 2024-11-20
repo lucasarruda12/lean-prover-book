04e207e51a2e7dea1401937ca5c40e3ef86e3909
LucasArruda12 <lucas123manoel4@gmail.com>
2024-11-20 10:06:18 -0300

    Props and proofs (1)

--- Lib/Propositions_and_Proofs.lean
+++ Lib/Propositions_and_Proofs.lean
@@ -60,18 +60,114 @@ example : (p ∨ q) ∨ r ↔ p ∨ (q ∨ r) :=
   )
 
 -- distributivity
-example : p ∧ (q ∨ r) ↔ (p ∧ q) ∨ (p ∧ r) := sorry
-example : p ∨ (q ∧ r) ↔ (p ∨ q) ∧ (p ∨ r) := sorry
+example : p ∧ (q ∨ r) ↔ (p ∧ q) ∨ (p ∧ r) :=
+  Iff.intro
+  (fun h : p ∧ (q ∨ r) =>
+    Or.elim h.right
+      (fun hq : q => Or.inl (And.intro h.left hq))
+      (fun hr : r => Or.inr (And.intro h.left hr))
+  )
+  (fun h : (p ∧ q) ∨ (p ∧ r) =>
+    Or.elim h
+      (fun hpq : p ∧ q =>
+        And.intro hpq.left (Or.inl hpq.right)
+      )
+      (fun hpr : p ∧ r =>
+        And.intro hpr.left (Or.inr hpr.right)
+      )
+  )
+example : p ∨ (q ∧ r) ↔ (p ∨ q) ∧ (p ∨ r) :=
+  Iff.intro
+  (fun h : p ∨ (q ∧ r) =>
+    Or.elim h
+    (fun hp : p => And.intro (Or.inl hp) (Or.inl hp))
+    (fun hqr : q ∧ r =>
+      And.intro
+      (Or.inr hqr.left)
+      (Or.inr hqr.right))
+  )
+  (fun h : (p ∨ q) ∧ (p ∨ r) =>
+    Or.elim h.left
+    (fun hp : p => Or.inl hp)
+    (fun hq : q =>
+      Or.elim h.right
+      (fun hp : p => Or.inl hp)
+      (fun hr : r => Or.inr (And.intro hq hr))
+    )
+  )
 
 -- other properties
-example : (p → (q → r)) ↔ (p ∧ q → r) := sorry
-example : ((p ∨ q) → r) ↔ (p → r) ∧ (q → r) := sorry
-example : ¬(p ∨ q) ↔ ¬p ∧ ¬q := sorry
-example : ¬p ∨ ¬q → ¬(p ∧ q) := sorry
-example : ¬(p ∧ ¬p) := sorry
-example : p ∧ ¬q → ¬(p → q) := sorry
-example : ¬p → (p → q) := sorry
-example : (¬p ∨ q) → (p → q) := sorry
-example : p ∨ False ↔ p := sorry
-example : p ∧ False ↔ False := sorry
-example : (p → q) → (¬q → ¬p) := sorry
+example : (p → (q → r)) ↔ (p ∧ q → r) :=
+  Iff.intro
+  (fun h : p → (q → r) =>
+    fun hpq : p ∧ q => h hpq.left hpq.right
+  )
+  (fun h : (p ∧ q) → r =>
+    fun hp : p => fun hq : q => h (And.intro hp hq)
+  )
+example : ((p ∨ q) → r) ↔ (p → r) ∧ (q → r) :=
+  Iff.intro
+  (fun h : ((p ∨ q) → r) =>
+    And.intro
+    (fun hp : p => h (Or.inl hp))
+    (fun hq : q => h (Or.inr hq))
+  )
+  (fun h : (p → r) ∧ (q → r) =>
+    fun hpq : p ∨ q =>
+    Or.elim hpq
+    (fun hp : p => h.left hp)
+    (fun hq : q => h.right hq)
+  )
+example : ¬(p ∨ q) ↔ ¬p ∧ ¬q :=
+  Iff.intro
+  (fun h : ¬(p ∨ q) =>
+    And.intro
+    (fun hp : p => h (Or.inl hp))
+    (fun hq : q => h (Or.inr hq))
+  )
+  (fun h : ¬p ∧ ¬q =>
+    fun hpq : p ∨ q =>
+      Or.elim hpq
+      (fun hp : p => h.left hp)
+      (fun hq : q => h.right hq)
+  )
+
+example : ¬p ∨ ¬q → ¬(p ∧ q) :=
+  fun h : ¬p ∨ ¬q =>
+  Or.elim h
+  (fun hnp : ¬p => fun hpq : p ∧ q => hnp hpq.left)
+  (fun hnq : ¬q => fun hpq : p ∧ q => hnq hpq.right)
+
+example : ¬(p ∧ ¬p) :=
+  fun h : p ∧ ¬p => h.right h.left
+
+example : p ∧ ¬q → ¬(p → q) :=
+  fun h : p ∧ ¬q =>
+    fun hpq : p → q => h.right (hpq h.left)
+
+example : ¬p → (p → q) :=
+  fun hnp : ¬p =>
+    fun hp : p => False.elim (hnp hp)
+
+example : (¬p ∨ q) → (p → q) :=
+  fun hnpq : ¬p ∨ q =>
+    fun hp : p =>
+      Or.elim hnpq
+      (fun hnp : ¬p => False.elim (hnp hp))
+      id
+
+example : p ∨ False ↔ p :=
+  Iff.intro
+  (fun hpf : p ∨ False =>
+    Or.elim hpf id False.elim
+  )
+  (Or.inl)
+
+example : p ∧ False ↔ False :=
+  Iff.intro
+  (fun hpf : p ∧ False => False.elim hpf.right)
+  (False.elim)
+
+example : (p → q) → (¬q → ¬p) :=
+  fun hpq : p → q =>
+    fun hnq : ¬q => fun hp : p => hnq (hpq hp)

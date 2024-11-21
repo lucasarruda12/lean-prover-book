261cc5510fc64e7da17d5ed17d989086cb2cc06a
LucasArruda12 <lucas123manoel4@gmail.com>
2024-11-20 22:30:47 -0300

    Quantifiers and Equality (2)

--- Quantifiers_and_Equality.lean
+++ Quantifiers_and_Equality.lean
@@ -1,5 +1,3 @@
--- Prove these equivalences:
-
 variable (α : Type) (p q : α → Prop)
 
 example : (∀ x, p x ∧ q x) ↔ (∀ x, p x) ∧ (∀ x, q x) :=
@@ -27,3 +25,42 @@ example : (∀ x, p x) ∨ (∀ x, q x) → ∀ x, p x ∨ q x :=
       Or.elim h
       (fun hpx : ∀ x, p x => Or.inl (hpx x))
       (fun hqx : ∀ x, q x => Or.inr (hqx x))
+
+variable (α : Type) (p q : α → Prop)
+variable (r : Prop)
+
+example : α → ((∀ x : α, r) ↔ r) :=
+  fun a : α =>
+    Iff.intro
+    (fun h : (∀ _ : α, r) => h a)
+    (fun h : r => fun _ : α => h)
+
+example : (∀ x, p x ∨ r) ↔ (∀ x, p x) ∨ r :=
+  Iff.intro
+  (fun h : (∀ x, p x ∨ r) =>
+    Or.elim (Classical.em r)
+    (Or.inr)
+    (fun hnr : ¬r =>
+      have hxpx : ∀x, p x :=
+        fun x : α =>
+          Or.elim (h x)
+          (id)
+          (fun hr : r => False.elim (hnr hr))
+      Or.inl hxpx
+    )
+  )
+  (fun h : (∀ x, p x) ∨ r =>
+    fun x : α =>
+      Or.elim h
+      (fun h : (∀ x, p x) => Or.inl (h x))
+      (Or.inr)
+  )
+
+example : (∀ x, r → p x) ↔ (r → ∀ x, p x) :=
+  Iff.intro
+  (fun h : ∀ x, r → p x =>
+    fun hr : r => fun x : α => h x hr
+  )
+  (fun h : (r → ∀ x, p x) =>
+    fun x : α => fun hr : r => h hr x
+  )
